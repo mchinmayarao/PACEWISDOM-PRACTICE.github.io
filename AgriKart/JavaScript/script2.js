e1378ab8fd65885cd702581dfcc7fcbf71a11e25
mchinmayarao <mchinmayarao@gmail.com>
2024-02-28 13:29:53 +0530

    total price calculation and issues in remove functions are fixed

--- AgriKart/JavaScript/script2.js
+++ AgriKart/JavaScript/script2.js
@@ -4,7 +4,8 @@ function addToCart(productName, productId, productImageSrc, productPrice) {
         productName: productName,
         productId: productId,
         productImageSrc: productImageSrc,
-        productPrice: productPrice
+        productPrice: productPrice,
+        quantity:1
     };
 
     var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
@@ -15,9 +16,15 @@ function addToCart(productName, productId, productImageSrc, productPrice) {
 
 var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
 
+function priceUpdate(index) {
+    let quantity = document.querySelector('.form-select[data-index="' + index + '"]').value;
+    cartItems[index].quantity = quantity;
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    location.reload();
 
-for(let i = 0; i<cartItems.length;i++) {
-    var cartItem = cartItems[i];
+}
+
+function renderCartItems(cartItem, i) {
     var cartItemElement = $('<div class="row gy-3 mb-4">' +
         '<div class="col-lg-5">' +
         '<div class="me-lg-5">' +
@@ -32,15 +39,15 @@ for(let i = 0; i<cartItems.length;i++) {
         '</div>' +
         '<div class="col-lg-2 col-sm-6 col-6 d-flex flex-row flex-lg-column flex-xl-row text-nowrap">' +
         '<div class="">' +
-        '<select style="width: 100px;" class="form-select me-4">' +
-        '<option>1</option>' +
-        '<option>2</option>' +
-        '<option>3</option>' +
-        '<option>4</option>' +
+        '<select style="width: 100px;" class="form-select me-4" data-index="' + i + '" onchange="priceUpdate(' + i +')">' +
+        '<option value="1" ' + (cartItem.quantity == 1 ? 'selected' : '') + '>1</option>' +
+        '<option value="2" ' + (cartItem.quantity == 2 ? 'selected' : '') + '>2</option>' +
+        '<option value="3" ' + (cartItem.quantity == 3 ? 'selected' : '') + '>3</option>' +
+        '<option value="4" ' + (cartItem.quantity == 4 ? 'selected' : '') + '>4</option>' +
         '</select>' +
         '</div>' +
         '<div class="">' +
-        '<text class="h6"> ₹ ' + cartItem.productPrice + '</text> <br />' +
+        '<text class="h6" id = "totPrice_' + i + '"> ₹ ' + (cartItem.productPrice * cartItem.quantity) + '</text> <br />' +
         '<small class="text-muted text-nowrap">₹ ' + cartItem.productPrice + ' / per item </small>' +
         '</div>' +
         '</div>' +
@@ -51,11 +58,40 @@ for(let i = 0; i<cartItems.length;i++) {
         '</div>' +
         '</div>');
 
-    $('#cartItems').append(cartItemElement);
+    return cartItemElement;
+
 }
 
+for (let i = 0; i < cartItems.length; i++) {
+
+    cartItemElement = renderCartItems(cartItems[i], i);
+
+    $('#cartItems').append(cartItemElement);
+
+
+}
+
+
+function totPrice_cal(){
+    var totPrice = 0;
+    for (let i = 0; i < cartItems.length; i++) {
+        totPrice += (cartItems[i].productPrice * cartItems[i].quantity);
+    }
+
+    return totPrice;
+}
 function removeFromCart(index) {
     cartItems.splice(index, 1);
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
-    location.reload(); // Refresh the page to reflect the updated cart
+    location.reload();
 }
+
+var totPrice = totPrice_cal();
+var discount = parseFloat((0.05 * totPrice).toFixed(2));
+var tax = parseFloat((0.18 * totPrice).toFixed(2));
+var finalPrice = totPrice - discount + tax;
+document.getElementById("totPrice").innerHTML = "₹ " + totPrice;
+document.getElementById("Discount").innerHTML = "₹ - " + discount;
+document.getElementById("tax").innerHTML = "₹ + " + tax;
+document.getElementById("finalPrice").innerHTML = "₹ + " + finalPrice;
+

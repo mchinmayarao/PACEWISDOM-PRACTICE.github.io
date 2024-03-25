6da7c8dde246b29fbbfc0678909d5e51fc490633
mchinmayarao <mchinmayarao@gmail.com>
2024-03-25 17:10:12 +0530

    pagination fixed

--- Exercise/ES6_ARRAY/script.js
+++ Exercise/ES6_ARRAY/script.js
@@ -1,18 +1,28 @@
-var dataJson = {};
 
-$.getJSON('movies.json', function(data) {
-    
-    dataJson = data;
-    
-    var dataLength = Object.entries(data).length;
-   // console.log(dataLength);
+const itemsPerPage = 5;
+let currentPage = 1;
+let jsonData = [];
+let maxButtons = 5;
 
-    var noOfPages = Math.ceil(dataLength/5);
+// Fetch JSON data from the file
+fetch('movies.json')
+    .then(response => response.json())
+    .then(data => {
+        jsonData = data;
+        displayData(currentPage);
+        renderPaginationButtons(currentPage);
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
 
-    
+function displayData(page) {
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const pageData = jsonData.slice(startIndex, endIndex);
 
-    data.forEach(function(movie,i) {
-        var sno = i+1;
+    const container = $('.data-container');
+    container.empty();
+    pageData.forEach(function (movie, i) {
+        var sno = i + 1;
         var title = movie.title;
         var imdbRating = movie.imdb.rating;
         var tomatoesRating = movie.tomatoes.viewer.rating;
@@ -23,25 +33,59 @@ $.getJSON('movies.json', function(data) {
         console.log("IMDb Rating:", imdbRating);
         console.log("Tomatoes Rating:", tomatoesRating);
         console.log("Plot:", plot);
-    
-        var row = $( '<tr class="text-center">' +
-        '<th id="sno" scope="row">' + sno + '</th>'+
-        '<td id="title">' + title + '</td>' +
-        '<td id="imdb">' + imdbRating + '</td>' +
-        '<td id="rottenTomatoes">' +tomatoesRating + '</td>' +
-        '<td id="View">' +plot + '</td>' +
-        '</tr>' )
-    
+
+        var row = $('<tr class="text-center">' +
+            '<th id="sno" scope="row">' + sno + '</th>' +
+            '<td id="title">' + title + '</td>' +
+            '<td id="imdb">' + imdbRating + '</td>' +
+            '<td id="rottenTomatoes">' + tomatoesRating + '</td>' +
+            '<td id="rottenTomatoes">' + ((tomatoesRating+imdbRating)/2).toFixed(1) + '</td>' +
+            '<td id="View">' + plot + '</td>' +
+            '</tr>')
+
         $('tbody').append(row);
-    
-      });
 
-})
+    });
+}
 
-function addPageNumber(num){
+function renderPaginationButtons() {
+    const paginationContainer = $('.pageNum');
+    paginationContainer.empty();
 
-    for(var i =0; i<num; i++){
-        var btn = $('<li class="page-item"><button class="page-link btn btn-link pageNumber" id="btn'+(i+1) + '">' + (i+1) + '</button></li>');
-        $('.pageNum').append(btn);
+    const numPages = Math.ceil(jsonData.length / itemsPerPage);
+    let startPage = Math.max(1, currentPage - Math.floor(maxButtons / 2));
+    let endPage = Math.min(numPages, startPage + maxButtons - 1);
+
+    startPage = Math.max(1, endPage - maxButtons + 1);
+
+    for (let i = startPage; i <= endPage; i++) {
+        const btn = $('<li class="page-item"><button class="page-link pageNumber">' + i + '</button></li>');
+        if (i === currentPage) {
+            btn.addClass('active');
+        }
+        btn.find('.pageNumber').click(function() {
+            currentPage = i;
+            displayData(currentPage);
+            renderPaginationButtons();
+        });
+        paginationContainer.append(btn);
     }
 }
+
+$('#previousPage').click(function() {
+    if (currentPage > 1) {
+        currentPage--;
+        displayData(currentPage);
+        renderPaginationButtons();
+    }
+});
+
+$('#nextPage').click(function() {
+    const numPages = Math.ceil(jsonData.length / itemsPerPage);
+    if (currentPage < numPages) {
+        currentPage++;
+        displayData(currentPage);
+        renderPaginationButtons();
+    }
+});
+

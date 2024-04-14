b6a0a177c9d9a546baf70eaf37bebc3fdfd233d3
mchinmayarao <mchinmayarao@gmail.com>
2024-04-14 18:23:52 +0530

    implementaion of ketamine clinic using tailwindcss, home section is completed

---
+++ Ketamine_Clinic/scriptTW.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+
+    mobileMenuToggle.addEventListener("click", function (event) {
+        event.stopPropagation();
+        mobileMenu.classList.toggle("hidden");
+    });
+
+
+    document.body.addEventListener("click", function (event) {
+
+        if (!mobileMenu.contains(event.target) && !mobileMenuToggle.contains(event.target)) {
+            mobileMenu.classList.add("hidden");
+        }
+    });
+});
---
+++ Ketamine_Clinic/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        colors: {
+          customGreen: '#81bcba',
+        },
+        
+      },
+    },
+    plugins: [],
+  }
+  

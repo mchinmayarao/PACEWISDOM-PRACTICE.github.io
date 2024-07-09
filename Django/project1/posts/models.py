469cddc8427d551818d8e659d8aca8ef40b87ca2
mchinmayarao <mchinmayarao@gmail.com>
2024-07-09 16:01:28 +0530

    Django Admin: line for registering the post into the admin, returning posts as object to the calling function, created the templete for viewing the post

--- Django/project1/posts/models.py
+++ Django/project1/posts/models.py
@@ -6,3 +6,7 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"title: {self.title}\nbody: {self.body}\nslug: {self.slug}\ndate: {self.date}\n"
+    

606e0c2054ddac2a177249f70ef18ee66fc17306
mchinmayarao <mchinmayarao@gmail.com>
2024-07-16 12:20:02 +0530

    ToDo application is completed

--- Django/toDoList/toDo/views.py
+++ Django/toDoList/toDo/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import ToDoList
 # Create your views here.
 def index(request):
-    return render(request , 'index.html')
+   if request.method == 'POST':
+      new_todo = ToDoList(item = request.POST['item'])
+      new_todo.save()
+      return redirect('/')
+   
+   return render(request,'index.html',{"todos":ToDoList.objects.all()})
+
+def delete(request,itemId):
+   item = ToDoList(id = itemId)
+   item.delete()
+   return redirect('/')

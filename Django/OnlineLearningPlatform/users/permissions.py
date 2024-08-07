7ba3bf37f9025f84a2376a74b27aba8ba80e3798
mchinmayarao <mchinmayarao@gmail.com>
2024-08-07 09:44:42 +0530

    fix: allow unauthenticated users to view courses
    - Updated IsTeacherOrReadOnly permission to allow read-only access to courses for unauthenticated users.
    - Ensured that course and content views are accessible without authentication for read operations.

--- Django/OnlineLearningPlatform/users/permissions.py
+++ Django/OnlineLearningPlatform/users/permissions.py
@@ -10,10 +10,10 @@ class IsTeacher(BasePermission):
 
 class IsTeacherOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
         if request.method in SAFE_METHODS:
             return True
+        if not request.user.is_authenticated:
+            return False
         return request.user.role == 'teacher'
 
 class IsOwnerOrReadOnly(BasePermission):
